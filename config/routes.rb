0697c190f7191b18ca8b009271f7e3c06e21e03b
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-08-10 00:49:09 +0900

    [Finish]AdvancedWork4 Follow

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'book_comments/creare'
-  get 'book_comments/destroy'
-  get 'favorites/create'
-  get 'favorites/destroy'
   devise_for :users
 
-  resources :users, only: [:show,:index,:edit,:update]
+  resources :users, only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'follows' => 'relationships#follower', as: 'follows'
+    get 'followers' => 'relationships#followed', as: 'followers'
+  end
   resources :books, except: [:new] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
