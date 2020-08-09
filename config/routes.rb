62e408bb591f4385dcd1acb69f55c2b7ac089e5d
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-08-09 23:07:25 +0900

    [Finish]AdvancedWork3 Favorite Comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'book_comments/creare'
+  get 'book_comments/destroy'
+  get 'favorites/create'
+  get 'favorites/destroy'
   devise_for :users
 
-  resources :users,only: [:show,:index,:edit,:update]
-  resources :books
+  resources :users, only: [:show,:index,:edit,:update]
+  resources :books, except: [:new] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
 
   root 'home#top'
   get 'home/about'
