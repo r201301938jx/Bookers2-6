26793911c3d3426b3f4adef5c475a8d375d8ff3f
Yuzuru <yuzuru@YuzurunoAir.AirPort>
2020-08-10 13:07:46 +0900

    [Finish]AdvancedWork8 Mail

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
   get 'search/search'
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 
   resources :users, only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
